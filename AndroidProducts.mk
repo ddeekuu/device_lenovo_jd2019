c686db36349e3aab73773e31a53334ea6b304ec7
ddeekuu <dddekuu@gmail.com>
2020-12-26 01:59:59 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/corvus_jd2019.mk
+    $(LOCAL_DIR)/nad_jd2019.mk
 
 COMMON_LUNCH_CHOICES := \
-    corvus_jd2019-user \
-    corvus_jd2019-userdebug \
-    corvus_jd2019-eng
+    nad_jd2019-user \
+    nad_jd2019-userdebug \
+    nad_jd2019-eng
